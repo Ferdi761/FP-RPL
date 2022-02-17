26c1f22bbf59c0266b0a1ec01f44372f89365c19
afdalabdallah <afdalabdallahm@gmail.com>
2022-02-17 08:16:25 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const signupRoutes = require("./routes/signup.js");
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + "/public"));
+
+app.use("/signup", signupRoutes);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/home.html"));
+});
+
+// app.get()
+
+app.listen(PORT, () => console.log("Server is running"));
---
+++ routes/signup.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require("body-parser");
+const multer = require('multer');
+const router = express.Router();
+const app = express();
+const upload = multer();
+
+let users = []
+
+app.use(upload.array());
+app.use(express.static(__dirname+"/public"));
+
+router.get('/', (req, res)=>{
+    // console.log(users);
+    res.send(users);
+    // res.sendFile(path.join(__dirname, "../public/signup.html"));
+});
+
+
+router.post('/', (req,res) => {
+    const user = req.body;
+    users.push(user);
+    // // var body = JSON.stringify(req.body);
+    // console.log(user);
+    res.send(`User with username ${user.username} added!`);
+})
+
+//search
+router.get('/:username', (req, res) => {
+    const { username } = req.params;
+    const foundUser = users.find((user) => user.username === username)
+    res.send(foundUser);
+})
+
+router.delete('/:username', (req, res) => {
+    const { username } = req.params;
+    users = users.filter((user) => user.username != username);
+    res.send(`User with username ${username} is deleted!`);
+});
+
+router.patch('/:username', (req, res) => {
+    const { username } = req.params;
+    const { firstName, lastName, Email, password} = req.body;
+    const user = users.find((user) => user.username === username);
+    if(firstName){
+        user.firstName = firstName;
+    }
+    if(lastName){
+        user.lastName = lastName;
+    }
+    if(Email){
+        user.Email = Email;
+    }
+    if(password){
+        user.password = password;
+    }
+
+    res.send(`User with username ${username} has updated his account!`)
+    
+})
+
+module.exports = router;
