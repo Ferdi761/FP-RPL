72616ef77f31fe31b4e2104d34ee85d93d2545e1
afdalabdallah <afdalabdallahm@gmail.com>
2022-02-22 01:42:07 +0700

    New mongoDB project

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 const express = require("express");
+const { default: mongoose } = require("mongoose");
 const path = require("path");
-const signupRoutes = require("./routes/signup.js");
 
 const app = express();
-const PORT = 5000;
-
 app.use(express.urlencoded({extended : true}));
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 
-app.use("/signup", signupRoutes);
+const start = async() => {
+    mongoose.connect(
+      'mongodb+srv://afdalabdallah:abdallah25@cluster0.nud19.mongodb.net/user_account?retryWrites=true&w=majority'
+    );
+    app.listen(3000, () => {
+      console.log('Listening on http://localhost:4000');
+    })
+};
 
-app.get("/", (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, "public/home.html"));
-});
+start();
+// app.use("/signup", signupRoutes);
+
+// app.get("/", (req, res) => {
+//   res.status(200).sendFile(path.join(__dirname, "public/home.html"));
+// });
 
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
